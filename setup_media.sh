3b4973ad8ed3f611746aedb63d64b6e35c06081d
Michael Fatemi <myfatemi04@gmail.com>
2024-12-11 00:40:50 -0500

    add images

---
+++ setup_media.sh
@@ -0,0 +1,6 @@
+# images/shorts.png
+
+ANKI_COLLECTION_DIR="/Users/michaelfatemi/Library/Application Support/Anki2/User 1/collection.media"
+MEDIA_DIR=$(pwd)/media
+
+ln -s $MEDIA_DIR _language_learning
